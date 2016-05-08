140bda3cb572af208233359a0111b05762272abc
Ana Jacimovic <Ana Jacimovic@Ana-PC>
2016-05-08 02:06:20 +0200

    Dokumentovane su klase FilmTableModel, SerijeTableModel i KorisnikTableModel.

--- BazaFilmovaISerija/src/bioskop/gui/SerijeTableModel.java
+++ BazaFilmovaISerija/src/bioskop/gui/SerijeTableModel.java
@@ -7,11 +7,22 @@ import javax.swing.table.AbstractTableModel;
 
 import projekcije.filmovi.Film;
 import projekcije.serije.Serija;
-
+/**
+ * 
+ * @author Ana Jacimovic, Jana Djurovic i Olivera Kordic
+ * 
+ * Model tabele koja prikazuje serije koji se dodaju u listu.
+ */
 public class SerijeTableModel extends AbstractTableModel{
-	
+	/**
+	 * Lista koja imenuje kolone tabele.
+	 */
 	private final String[] kolone = new String[] {"Ime serije", "Ocena serije", "Trajanje serije"};
 	private LinkedList<Serija> listaSerija;
+	/**
+	 * Konstuktor klase SerijeTableModel
+	 * @param listaSerija
+	 */
 	
 	public SerijeTableModel(LinkedList<Serija> listaSerija) {
 		if (listaSerija == null) {
@@ -20,16 +31,23 @@ public class SerijeTableModel extends AbstractTableModel{
 			this.listaSerija = listaSerija;
 		}
 	}
+	/**
+	 * Funkcija koja vraca broj kolona tabele.
+	 */
 	@Override
 	public int getColumnCount() {
 		return kolone.length;
 	}
-
+	/**
+	* Funkcija koja vraca broj redova tabele.
+	*/
 	@Override
 	public int getRowCount() {
 		return listaSerija.size();
 	}
-
+	/**
+	 * Funkcija koja vraca vrednost iz kolone i reda koji su prosledjeni kao parametri funkciji.
+	 */
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		Serija s = listaSerija.get(rowIndex);
